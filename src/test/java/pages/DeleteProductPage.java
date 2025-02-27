ea86b674bfec3d32388a999a194b2a8512f3b454
TazoPsuturi <tazopsuturi33@gmail.com>
2025-02-27 23:22:13 +0400

    added functionality of delete product page

--- src/test/java/pages/DeleteProductPage.java
+++ src/test/java/pages/DeleteProductPage.java
@@ -1,7 +1,24 @@
 package pages;
 
+import aquality.selenium.elements.interfaces.IButton;
 import aquality.selenium.forms.Form;
+import org.openqa.selenium.By;
 
 public class DeleteProductPage extends Form {
-
+	private final IButton deleteProduct = getElementFactory().getButton(By.cssSelector("data-cy[='delete-button']"), "delete product");
+	private final IButton confirmDeleteProduct = getElementFactory().getButton(By.cssSelector("data-cy[='confirm-delete-button']"), "confirm delete");
+	
+	public DeleteProductPage() {
+		super(By.cssSelector("data-cy[='delete-button']"), "delete button");
+	}
+	
+	public void clickDeleteProductButton() {
+		deleteProduct.click();
+	}
+	
+	public void clickConfirmDeleteProductButton() {
+		confirmDeleteProduct.state().waitForDisplayed();
+		confirmDeleteProduct.click();
+	}
+	
 }
