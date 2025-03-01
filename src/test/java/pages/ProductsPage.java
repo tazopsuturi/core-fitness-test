8fbf74a83c175c08466b9c7430676144a918c662
TazoPsuturi <tazopsuturi33@gmail.com>
2025-03-01 22:45:28 +0400

    added new files, modified products page class

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -5,12 +5,14 @@ import aquality.selenium.elements.interfaces.IButton;
 import aquality.selenium.elements.interfaces.ILabel;
 import aquality.selenium.forms.Form;
 import org.openqa.selenium.By;
+import utils.ConfigReader;
+import utils.SettingsTestData;
 
 import java.time.Duration;
 import java.util.List;
 
 public class ProductsPage extends Form {
-	private static final By product = By.xpath("//div[@class=\"flex flex-col items-center rounded-lg shadow-lg w-full sm:w-80 bg-white dark:bg-gray-800 overflow-hidden group duration-100 ease-in-out transition-transform transform hover:scale-105\"]");
+	private static final By product = By.xpath("//div[@class='flex flex-col items-center rounded-lg shadow-lg w-full sm:w-80 bg-white dark:bg-gray-800 overflow-hidden group duration-100 ease-in-out transition-transform transform hover:scale-105']");
 	private final IButton addProduct = getElementFactory().getButton(By.cssSelector("[data-cy='add-product-button']"), "Add Product Button");
 	
 	public ProductsPage() {
@@ -24,11 +26,10 @@ public class ProductsPage extends Form {
 	public void selectProductsToDelete() {
 		AqualityServices.getConditionalWait().waitFor(
 				() -> !getElementFactory().findElements(product, ILabel.class).isEmpty(),
-				Duration.ofSeconds(10)
+				Duration.ofSeconds(SettingsTestData.getEnvData().getWait())
 		);
 		
 		List<ILabel> products = getElementFactory().findElements(product, ILabel.class);
-		AqualityServices.getLogger().info("Total products found: " + products.size());
 		
 		for (ILabel product : products) {
 			ILabel viewButton = product.findChildElement(
@@ -42,7 +43,7 @@ public class ProductsPage extends Form {
 			
 			String titleText = productTitle.getText().toLowerCase();
 			
-			if (titleText.contains("test")) {
+			if (titleText.contains(ConfigReader.config.product.enTitle)) {
 				AqualityServices.getConditionalWait().waitFor(() -> viewButton.state().isDisplayed() && viewButton.state().isClickable(), Duration.ofSeconds(10));
 				viewButton.click();
 				break;
