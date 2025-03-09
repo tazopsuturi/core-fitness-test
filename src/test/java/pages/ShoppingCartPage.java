b619b3e6059a8e01ec28845d470bbd7f98cc436f
TazoPsuturi <tazopsuturi33@gmail.com>
2025-03-09 22:10:31 +0400

    modified pom file

--- src/test/java/pages/ShoppingCartPage.java
+++ src/test/java/pages/ShoppingCartPage.java
@@ -9,8 +9,6 @@ public class ShoppingCartPage extends Form {
 	private final ILabel emptyCartMsg = getElementFactory().getLabel(By.xpath("//div[@class='text-center text-gray-500 py-8']"), "Empty Cart Message");
 	private final IButton clearCart = getElementFactory().getButton
 			(By.xpath("//button[contains(@class, 'inline-flex') and contains(@class, 'rounded-md') and contains(@class, 'hover:bg-red-50')]"), "Clear Cart Button");
-	private final IButton checkoutNow = getElementFactory().getButton
-			(By.xpath("//button[contains(@class, 'inline-flex') and contains(@class, 'rounded-md') and contains(@class, 'bg-green-600')]"), "Checkout Now Button");
 	
 	public ShoppingCartPage() {
 		super(By.xpath("//h2[@class='tracking-tight text-2xl font-bold']"), "Shopping Cart Page");
